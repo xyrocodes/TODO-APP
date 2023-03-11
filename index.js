5c4e57771f2c8bec2f49b631674ff9731bcfdf3e
anurag <anurag@xyrocodes.com>
2023-03-11 16:03:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+port = 9000;
+const expressLayout = require('express-ejs-layouts');
+app.use(expressLayout)
+const db = require('./config/mongoose')
+app.use(express.urlencoded()) 
+app.use('/',require('./routes'))
+app.use(express.static('./assets')) // for getting static
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true)
+app.set('view engine','ejs');
+app.set('views','./views')
+app.listen(port,function(err){
+    if(err){
+        console.log(`error in running the ${port}`)
+        return;
+    }
+    console.log(`Server is running @ ${port}`)
+})
